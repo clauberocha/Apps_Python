4371d383c925a2a78a1b33e6751084242b67b876
clauberocha <clauberocha@gmail.com>
2023-10-24 15:42:56 -0300

    nova versao

--- Aula 04.py
+++ Aula 04.py
@@ -3,22 +3,35 @@
 # CRIAR PROGRAMAS COM CONDICIONAL E LAÇOS DE REPETIÇÃO
 # COMANDOS - FOR
 # COMANDO - WHILE
-# COMANDO - RANGE (PARA GERAR NUMEROS SEQUENCIAIS
+# COMANDO - RANGE (PARA GERAR NUMEROS SEQUENCIAIS)
 
 #EX-01 - COMANDO "FOR"
 # for x in range(90, 101):
 #     print(x)
 
 #EX-02 DESCOBRIR SE O NUMERO É PRIMO OU NAO
-a = int(input('Entre com o número: '))
-div = 0
-for x in range (1, a+1):
-    resto = a % x
-    print(a, resto)
-    if resto == 0:
-        div = div + 1
+#a = int(input('Entre com o número: '))
+#div = 0
+#for x in range (1, a+1):
+#   resto = a % x
+#   print(a, resto)
+#   if resto == 0:
+#       div = div + 1
 
-if div == 2:
-    print('Número {} é primo'.format(a))
-else:
-    print('número {} não é primo'.format(a))
+#if div == 2:
+#    print('Número {} é primo'.format(a))
+#else:
+#    print('número {} não é primo'.format(a))
+
+#EX-03 NUMEROS PRIMOS DE 1  a 100
+a = int(input('Entre com um valor: '))
+for num in range(a):
+    div = 0
+    for x in range (1, num+1):
+        resto = num % x
+        #print(x, resto)
+        if resto == 0:
+            div += 1
+    if div == 2:
+        print(num)
+    
